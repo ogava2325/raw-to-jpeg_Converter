1d669b30a64acdefa3e907e5a958fdfd0bbdea3a
ogava_2325 <dimapajuk2325@gmail.com>
2023-10-03 19:33:38 +0300

    Add project files.

---
+++ Andrew/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Andrew
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectPathButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SelectPathButton
+            // 
+            this.SelectPathButton.Location = new System.Drawing.Point(12, 12);
+            this.SelectPathButton.Name = "SelectPathButton";
+            this.SelectPathButton.Size = new System.Drawing.Size(105, 53);
+            this.SelectPathButton.TabIndex = 0;
+            this.SelectPathButton.Text = "Select file";
+            this.SelectPathButton.UseVisualStyleBackColor = true;
+            this.SelectPathButton.Click += new System.EventHandler(this.SelectPathButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(12, 71);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(105, 53);
+            this.SaveButton.TabIndex = 2;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.SelectPathButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectPathButton;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
+
---
+++ Andrew/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageMagick;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Andrew
+{
+    public partial class Form1 : Form
+    {
+        private string inputFilePath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void SelectPathButton_Click(object sender, EventArgs e)
+        {
+            inputFilePath = SelectConvertPath();
+
+            if (string.IsNullOrEmpty(inputFilePath))
+            {
+                MessageBox.Show("You didn't select the file!");
+            }
+        }
+
+        //selecting where to save a converted file
+        private string SelectSavePath()
+        {
+            string resultPath = string.Empty;
+
+            // Create an instance of the SaveFileDialog
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // Set the file filter and default extension (optional)
+            saveFileDialog.Filter = "JPEG files (*.jpg;*.jpeg)|*.jpg;*.jpeg|All files (*.*)|*.*";
+            saveFileDialog.DefaultExt = "jpg";
+
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                resultPath = saveFileDialog.FileName;
+            }
+
+            return resultPath;
+        }
+
+        //selecting file you want to convert
+        private string SelectConvertPath()
+        {
+            string resultPath = string.Empty;
+
+            // Create an instance of the OpenFileDialog
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            //Select a title for a dialog window
+            openFileDialog1.Title = "Select File";
+
+            //Select a filter for formats of files
+            openFileDialog1.Filter = "DNG and CR2 Files | *.dng; *.cr2 | All Files | *.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.ShowDialog();
+
+            if (!string.IsNullOrEmpty(openFileDialog1.FileName))
+            {
+                resultPath = openFileDialog1.FileName;
+            }
+
+            return resultPath;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string saveFilePath = SelectSavePath();
+
+            // Show the dialog to let the user select the save location and file name
+            if (string.IsNullOrEmpty(saveFilePath))
+            {
+                MessageBox.Show("Input file path is empty or null.");
+                return;
+            }
+
+            using (MagickImage image = new MagickImage(inputFilePath))
+            {
+                // Set the output format to JPEG and specify the compression quality (0-100)
+                image.Format = MagickFormat.Jpeg;
+                image.Quality = 10; // Adjust the quality as needed
+
+                // Save the converted image to the selected file path
+                image.Write(saveFilePath);
+
+                MessageBox.Show("Image saved successfully.");
+            }
+        }
+    }
+}
